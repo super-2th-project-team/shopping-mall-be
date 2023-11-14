d5ea849a4415be188ce1e2313fe5cf9a27f8c3a1
Kim Jae Hyuk <rlawogur816@gmail.com>
2023-11-14 10:40:04 +0900

    make prj

---
+++ prj2/src/test/java/com/be01/prj2/Prj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be01.prj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
