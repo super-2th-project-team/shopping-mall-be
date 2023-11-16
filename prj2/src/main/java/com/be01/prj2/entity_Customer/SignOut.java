478592240b11b9319b80ce6247a8fca8a216a400
Kim Jae Hyuk <rlawogur816@gmail.com>
2023-11-17 00:12:30 +0900

    Squashed commit of the following:
    commit 8da76f3395a5bc29c73810dec29cafcf9340d7d6
    Author: Kim Jae Hyuk <rlawogur816@gmail.com>
    Date:   Fri Nov 17 00:04:41 2023 +0900
    
        kk
    
    commit 7a5499bccec1e2e08047f7cebbc79b509e025fcc
    Author: Kim Jae Hyuk <rlawogur816@gmail.com>
    Date:   Thu Nov 16 23:55:43 2023 +0900
    
        Squashed commit of the following:
    
        commit fddc12096c099a134f3925afb99a2c8cd7d53907
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 23:47:23 2023 +0900
    
            pkg명변경
    
        commit cb43d8fc1abfa32e17c4426afdb29ca74d5d8e67
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 23:42:29 2023 +0900
    
            추가정보입력(성별, 주소)
    
        commit 51d2c4270eea00c3eb41c5eed1a237b34301fc32
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 21:48:02 2023 +0900
    
            / 추가,,
    
        commit e73693ff392e4df8d54a8bd9f204373bb808adc5
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 01:17:11 2023 +0900
    
            DB 내용 환경변수로 추가
    
        commit 40627d6cdbc713187b8f73db538c4002865633bf
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 01:00:22 2023 +0900
    
            테스트 컨트롤러 구성
    
        commit 0f02073c5bd05deec3c168a562ff72e1a11e02a7
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 00:58:24 2023 +0900
    
            탈퇴 구현, angMatchers 추가
    
        commit be4129eba01cdb9503fb2992f7707fc120878464
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 00:34:56 2023 +0900
    
            회원탈퇴 개발 중
    
        commit 4835a28122d7706a00564008390952e818e2f73b
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Wed Nov 15 17:26:35 2023 +0900
    
            login 구현
    
        commit e18468b5547fdc3d561086572294e177f7cdc305
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Wed Nov 15 00:55:18 2023 +0900
    
            create token 구현 중

---
+++ prj2/src/main/java/com/be01/prj2/entity_Customer/SignOut.java
@@ -0,0 +1,26 @@
+package com.be01.prj2.entity_Customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "sign_out_customer")
+public class SignOut {
+
+    @Id
+    @Column(name = "email")
+    private String email;
+    private String mobile;
+
+}
