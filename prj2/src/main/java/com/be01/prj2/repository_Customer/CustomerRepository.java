42f287cea384b935ce6e1ba909e26fcbb05f0603
Kim Jae Hyuk <rlawogur816@gmail.com>
2023-11-17 00:15:52 +0900

    Squashed commit of the following:
    commit 8da76f3395a5bc29c73810dec29cafcf9340d7d6
    Author: Kim Jae Hyuk <rlawogur816@gmail.com>
    Date:   Fri Nov 17 00:04:41 2023 +0900
    
        kk
    
    commit 7a5499bccec1e2e08047f7cebbc79b509e025fcc
    Author: Kim Jae Hyuk <rlawogur816@gmail.com>
    Date:   Thu Nov 16 23:55:43 2023 +0900
    
        Squashed commit of the following:
    
        commit fddc12096c099a134f3925afb99a2c8cd7d53907
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 23:47:23 2023 +0900
    
            pkg명변경
    
        commit cb43d8fc1abfa32e17c4426afdb29ca74d5d8e67
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 23:42:29 2023 +0900
    
            추가정보입력(성별, 주소)
    
        commit 51d2c4270eea00c3eb41c5eed1a237b34301fc32
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 21:48:02 2023 +0900
    
            / 추가,,
    
        commit e73693ff392e4df8d54a8bd9f204373bb808adc5
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 01:17:11 2023 +0900
    
            DB 내용 환경변수로 추가
    
        commit 40627d6cdbc713187b8f73db538c4002865633bf
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 01:00:22 2023 +0900
    
            테스트 컨트롤러 구성
    
        commit 0f02073c5bd05deec3c168a562ff72e1a11e02a7
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 00:58:24 2023 +0900
    
            탈퇴 구현, angMatchers 추가
    
        commit be4129eba01cdb9503fb2992f7707fc120878464
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Thu Nov 16 00:34:56 2023 +0900
    
            회원탈퇴 개발 중
    
        commit 4835a28122d7706a00564008390952e818e2f73b
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Wed Nov 15 17:26:35 2023 +0900
    
            login 구현
    
        commit e18468b5547fdc3d561086572294e177f7cdc305
        Author: Kim Jae Hyuk <rlawogur816@gmail.com>
        Date:   Wed Nov 15 00:55:18 2023 +0900
    
            create token 구현 중

---
+++ prj2/src/main/java/com/be01/prj2/repository_Customer/CustomerRepository.java
@@ -0,0 +1,26 @@
+package com.be01.prj2.repository_Customer;
+
+import com.be01.prj2.entity_Customer.Customer;
+import com.be01.prj2.role_Customer.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByEmailAndRole(String email, Role role);
+
+    Optional<Customer> findAllByRole(Role role);
+
+    Optional<Customer> findAllByEmailAndPasswordAndMobile(String email,String password,String mobile);
+
+    Optional<Customer> findAllByEmailAndMobile(String email,String mobile);
+    Optional<Customer> findByEmailAndMobile(String email,String mobile);
+
+}
---
+++ prj2/src/main/java/com/be01/prj2/role_Customer/Role.java
@@ -0,0 +1,19 @@
+package com.be01.prj2.role_Customer;
+
+public enum Role {
+    USER("ROLE_USER"),
+
+    SELLER("ROLE_SELLER"),
+
+    SIGNOUT("ROLE_SIGNOUT");
+
+    private String name;
+
+    Role(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
