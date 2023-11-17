5601bb913a9413d69911461c8165446ad68b64bc
Kim Jae Hyuk <rlawogur816@gmail.com>
2023-11-17 10:37:20 +0900

    change pkg

--- prj2/src/main/java/com/be01/prj2/exception_Customer/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.be01.prj2.exception_Customer;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
---
+++ prj2/src/main/java/com/be01/prj2/repository/customUserDetails/CustomUserDetails.java
@@ -0,0 +1,26 @@
+package com.be01.prj2.repository.customUserDetails;
+
+import com.be01.prj2.entity.Customer;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.be01.prj2.repository.CustomerRepository;
+
+@Service
+public class CustomUserDetails implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomUserDetails(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("로그인 시도한 아이디가 없는 아이디 입니다"));
+
+        return customer;
+    }
+}
