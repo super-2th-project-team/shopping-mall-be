f2a355e6a954fcc51b58e49b59d5d1a7ea970d81
Kim Jae Hyuk <rlawogur816@gmail.com>
2023-11-20 22:21:43 +0900

    cart DTo 생성

---
+++ prj2/src/main/java/com/be01/prj2/dto/cartDto/CartDto.java
@@ -0,0 +1,17 @@
+package com.be01.prj2.dto.cartDto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Builder
+@AllArgsConstructor
+public class CartDto {
+
+    private Long cartId;
+    private Long buyerId;
+    
+
+}
---
+++ prj2/src/main/java/com/be01/prj2/dto/cartDto/CartProductDto.java
@@ -0,0 +1,4 @@
+package com.be01.prj2.dto.cartDto;
+
+public class CartProductDto {
+}
